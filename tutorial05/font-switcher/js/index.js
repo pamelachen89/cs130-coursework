1dc741d4d151d544421c5fdb2f9f2574a60e5f18
Pamela <pamelachen89@gmail.com>
2021-05-14 00:34:58 -0500

    adding my tutorial05 to the repo

---
+++ tutorial05/font-switcher/js/index.js
@@ -0,0 +1,11 @@
+const makeBigger = () => {
+  document.querySelector('.content').style.fontSize = '2em';
+};
+
+const makeSmaller = () => {
+   document.querySelector('.content').style.fontSize = '1.4em';
+};
+
+
+document.querySelector('.a1').onclick = makeBigger;
+document.querySelector('.a2').onclick = makeSmaller;
