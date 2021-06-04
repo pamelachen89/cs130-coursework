cd8331fc1b711f098be2112952d90bb2182f35ef
Pamela <pamelachen89@gmail.com>
2021-06-04 17:11:42 -0500

    uploading final project

---
+++ finalproject/js/scrripts.js
@@ -0,0 +1,11 @@
+//scroll to top
+var scrolltotop = document.getElementById("gototop");
+
+scrolltotop.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth'});
+
+//ask me anything submit
+
+// var asksubmit = document.getElementById("submit");
+
+// asksubmit.onclick = () =>
+    
